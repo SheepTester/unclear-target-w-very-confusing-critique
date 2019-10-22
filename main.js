b35b82ecb1467dee5f16ad7e71e68505b73a9db7
SheepTester <ussa.say@gmail.com>
2019-10-21 19:55:18 -0700

    allow going back

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const VOID = () => {}
+
 function createResolvablePromise () {
   let resolve
   const promise = new Promise(res => (resolve = res))
@@ -11,13 +13,43 @@ let height = window.innerHeight
 
 const MIN_TOP = 20
 function updateScenes () {
-  const base = (height + boundingBoxes.get(sceneOrder[0]).height) / 2
+  const base = (height + boundingBoxes.get(sceneOrder[0].elem).height) / 2
   let offset = 0
-  for (const scene of sceneOrder) {
-    const box = boundingBoxes.get(scene)
+  for (const {elem} of sceneOrder) {
+    const box = boundingBoxes.get(elem)
     offset += box.height
-    scene.style.top = (base - offset < MIN_TOP && scene === sceneOrder[0] ? MIN_TOP : base - offset) + 'px'
-    scene.style.left = ((width - box.width) / 2) + 'px'
+    elem.style.top = (base - offset < MIN_TOP && elem === sceneOrder[0].elem ? MIN_TOP : base - offset) + 'px'
+    elem.style.left = ((width - box.width) / 2) + 'px'
+  }
+}
+
+const sceneTypes = {
+  title: (entry, scene) => {
+    entry.elem.appendChild(Fragment([
+      Elem('div', {className: 'title'}, [scene.title]),
+      Elem('div', {className: 'subtitle'}, [scene.subtitle]),
+      Elem('div', {className: 'cover'}),
+      Elem('div', {
+        className: 'choice',
+        onclick: e => {
+          entry.setScene(scene.continue)
+          e.stopPropagation()
+        }
+      }, ['Continue'])
+    ]))
+  },
+  default: (entry, scene) => {
+    entry.elem.appendChild(Fragment([
+      Elem('div', {className: 'message'}, [scene.message]),
+      ...Object.entries(scene.choices || {}).map(([choiceMsg, choiceScene]) =>
+        Elem('div', {
+          className: 'choice',
+          onclick: e => {
+            entry.setScene(choiceScene)
+            e.stopPropagation()
+          }
+        }, [choiceMsg]))
+    ]))
   }
 }
 
@@ -28,57 +60,54 @@ fetch('./game.json')
     while (scene) {
       if (typeof scene === 'string') {
         scene = gameData[scene]
-        continue
       }
 
-      const sceneElem = Elem('div', {className: 'entry'})
       const {promise: sceneEndProm, resolve: done} = createResolvablePromise()
-      let ended = false
-
-      if (scene.special) {
-        switch (scene.special) {
-          case 'title': {
-            sceneElem.classList.add('has-title')
-            sceneElem.appendChild(Fragment([
-              Elem('div', {className: 'title'}, ['Pistole offers a wide range of opportunities.']),
-              Elem('div', {
-                className: 'choice',
-                onclick: e => {
-                  scene = scene.continue
-                  done()
-                }
-              }, ['Continue'])
-            ]))
-            break
-          }
-        }
+      let entry
+      if (scene.isEntry) {
+        entry = scene
+        scene = entry.scene
       } else {
-        sceneElem.appendChild(Fragment([
-          Elem('div', {className: 'message'}, [scene.message]),
-          ...Object.entries(scene.choices || {}).map(([choiceMsg, choiceScene]) =>
-            Elem('div', {
-              className: 'choice',
-              onclick: e => {
-                scene = choiceScene
-                done()
+        entry = {
+          isEntry: true,
+          elem: Elem('div', {
+            className: 'entry',
+            onclick: e => {
+              if (entry.elem.classList.contains('uninteractable')) {
+                let firstEntry = sceneOrder[0]
+                while (sceneOrder[0] && sceneOrder[0] !== entry) {
+                  document.body.removeChild(sceneOrder[0].elem)
+                  sceneOrder.shift()
+                }
+                firstEntry.setScene(entry)
+                updateScenes()
               }
-            }, [choiceMsg]))
-        ]))
+            }
+          }),
+          scene
+        }
+        sceneTypes[scene.special || 'default'](entry, scene)
+        document.body.appendChild(entry.elem)
+        sceneOrder.unshift(entry)
       }
 
-      document.body.appendChild(sceneElem)
-      sceneElem.focus()
+      entry.setScene = newScene => {
+        scene = newScene
+        done()
+      }
+      entry.elem.classList.remove('uninteractable')
+
       await Promise.all([
         sceneEndProm,
         new Promise(res => window.requestAnimationFrame(() => {
-          boundingBoxes.set(sceneElem, sceneElem.getBoundingClientRect())
-          sceneOrder.unshift(sceneElem)
+          boundingBoxes.set(entry.elem, entry.elem.getBoundingClientRect())
           updateScenes()
           res()
         }))
       ])
-      ended = true
-      sceneElem.classList.add('uninteractable')
+
+      entry.setScene = VOID
+      entry.elem.classList.add('uninteractable')
     }
   })
 
